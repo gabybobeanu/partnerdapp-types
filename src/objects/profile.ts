75ac8ab92aebd680342080c510ee8f13fd666687
GB <gaby.bobeanu@gmail.com>
2025-09-30 11:13:09 +0300

    fix profile ids

--- src/objects/profile.ts
+++ src/objects/profile.ts
@@ -1,15 +1,16 @@
 import { ProfileCode } from "../constants.js";
 import type { Profile } from "../index.js";
 
-export const PROFILE_RECRUITER: Profile = {
-    id: 1,
-    code: ProfileCode.RECRUITER,
-    name: "Recruiter",
-};
+
 export const PROFILE_EMPLOYER: Profile = {
-    id: 2,
+    id: 1,
     code: ProfileCode.EMPLOYER,
     name: "Employer",
+};
+export const PROFILE_RECRUITER: Profile = {
+    id: 2,
+    code: ProfileCode.RECRUITER,
+    name: "Recruiter",
 };
 export const PROFILE_CANDIDATE: Profile = {
     id: 3,
